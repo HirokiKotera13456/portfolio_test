8595a887b2a54d2d4c39f7fdaba522ed044ceffa
nijimajohn <86758487+nijimajohn@users.noreply.github.com>
2021-10-01 18:25:41 +0900

    [ログイン機能追加]

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  get 'dogs/index'
-  get 'dogs/edit'
-  get 'dogs/show'
   root "homes#top"
+  devise_for :users
+
+  resources :dogs, only: [:new, :create, :index, :show, :destroy, :edit, :update]
+
 end
